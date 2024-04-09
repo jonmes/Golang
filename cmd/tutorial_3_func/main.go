705731c17051253a448919bb569aa908239fdb4c
jonimes <jonimsfn@gmail.com>
2024-04-09 22:55:07 +0300

    function added

---
+++ cmd/tutorial_3_func/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+	printMe("print function!")
+	var result, remainder, err = intDivision(10, 0)
+
+	// if else statements
+
+	// if err != nil {
+	// 	fmt.Printf(err.Error())
+	// } else if remainder == 0 {
+	// 	fmt.Printf("The result of the integer division is %d\n", result)
+	// } else {
+	// 	fmt.Printf("result: %v, remainder: %d\n", result, remainder)
+	// }
+
+	// switch statement
+
+	switch remainder {
+	case 0:
+		fmt.Printf(err.Error())
+	case 1, 2:
+		fmt.Printf("The result of the integration division is %d\n", result)
+	default:
+		fmt.Printf("result: %v, remainder: %v\n", result, remainder)
+	}
+}
+
+// This function takes a string as an argument and prints it; this function doesn't return any value
+func printMe(printValue string) {
+	fmt.Println(printValue)
+}
+
+// This function takes two integers as arguments and returns their sum and remainder
+func intDivision(a int, b int) (int, int, error) {
+
+	var err error
+	if b == 0 {
+		err = errors.New("division by zero is not allowed ")
+		return 0, 0, err
+	}
+	var result int = a / b
+	var remainder int = a % b
+	return result, remainder, err
+}
